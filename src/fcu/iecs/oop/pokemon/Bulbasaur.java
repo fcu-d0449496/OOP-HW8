a1fefe85d204ec5b2e7ef5c8f40ebfa2b65176e5
johnn <johnn@DESKTOP-GK454JI>
2016-12-23 13:54:57 +0800

    OOP-HW8

---
+++ src/fcu/iecs/oop/pokemon/Bulbasaur.java
@@ -0,0 +1,11 @@
+package fcu.iecs.oop.pokemon;
+
+public class Bulbasaur extends Pokemon {
+
+	public Bulbasaur(String nickName, PokemonType type, int cp) {
+		super(nickName, type, cp);
+	}
+	public void attack(){
+		System.out.println("Tackle¡K");
+	}
+}
